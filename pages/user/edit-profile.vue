512157eee5586e5738466b7d8acdeecaf77633ea
Toby Travis <tobytravis@tobys-mbp.home>
2022-01-09 16:16:16 +0000

    Adding container to some pages

--- pages/user/edit-profile.vue
+++ pages/user/edit-profile.vue
@@ -1,16 +1,21 @@
 <template>
-  <div>
+  <div class="container">
     <BaseFormFeedback v-if="feedback.message !== ''" :type="feedback.type">
-      {{
-      feedback.message
-      }}
+      {{ feedback.message }}
     </BaseFormFeedback>
     <div class="grid grid-cols-12 gap-4">
       <div class="col-span-12 sm:col-span-6">
-        <BaseTitle :level="2" :has-decoration="true" class="text-xl">Profile details</BaseTitle>
+        <BaseTitle :level="2" :has-decoration="true" class="text-xl"
+          >Profile details</BaseTitle
+        >
         <div class="form-section">
           <form @submit.prevent="updateProfile">
-            <BaseInput id="userName" v-model="user.userName" label="Username" type="text" />
+            <BaseInput
+              id="userName"
+              v-model="user.userName"
+              label="Username"
+              type="text"
+            />
             <div class="form-section-buttons">
               <BaseButton type="submit" button-class="bg-southern text-white">
                 <slot>Update Profile</slot>
@@ -20,7 +25,9 @@
         </div>
       </div>
       <div class="col-span-12 sm:col-span-6">
-        <BaseTitle :level="2" :has-decoration="true" class="text-xl">Update login details</BaseTitle>
+        <BaseTitle :level="2" :has-decoration="true" class="text-xl"
+          >Update login details</BaseTitle
+        >
         <div class="form-section">
           <form @submit.prevent="updateEmail">
             <BaseInput
@@ -63,6 +70,9 @@ export default {
       return user
     },
   },
+  mounted() {
+    this.$store.dispatch('pages/setPageTitle', 'Profile')
+  },
   methods: {
     async updateProfile() {
       let updateRef = await this.$store.dispatch('users/updateProfile', {
@@ -97,9 +107,6 @@ export default {
         }
       }
     },
-  },
-  mounted() {
-    this.$store.dispatch('pages/setPageTitle', 'Profile')
   },
 }
 </script>
--- pages/user/stats.vue
+++ pages/user/stats.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="container">
     <client-only>
       <template v-if="currentUserId === ''">
         <LoggedOutMessage />
